81dc32baec86670dc9c83896ace88d888a822c21
Jingsi Gong <gongjingsi@hotmail.com>
2022-07-11 21:31:18 -0400

    declare fight function and playName var with alert and log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
 window.alert("This is an alert! JavaScript is running!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+var playerName = window.prompt("What is your robot's name?");
+
+window.alert("Our robot's name is " + playerName);
+
+console.log("Our robot's name is " + playerName);
